72b45df8db3cafdf26ef83ee823b58a0756177ec
dawcr <dawcraz@gmail.com>
2025-01-02 18:49:45 +0100

    added metrics

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 package main
 
 func main() {
-	server := newServer()
-	startServer(server)
+	startServer()
 }
--- server.go
+++ server.go
@@ -1,20 +1,40 @@
 package main
 
-import "net/http"
-
-const (
-	port = "8080"
+import (
+	"log"
+	"net/http"
+	"sync/atomic"
 )
 
-func newServer() *http.Server {
+const (
+	port                 = "8080"
+	filepathRoot         = "."
+	filepathAPP          = "/app/"
+	filepathHealthz      = "/healthz"
+	filepathMetricz      = "/metrics"
+	filepathResetMetricz = "/reset"
+)
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func startServer() {
 	mux := http.NewServeMux()
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
+	mux.Handle(filepathAPP, apiCfg.middlewareMetricsInc(http.StripPrefix(filepathAPP, http.FileServer(http.Dir(filepathRoot)))))
+	mux.HandleFunc(filepathHealthz, handlerReadiness)
+	mux.HandleFunc(filepathMetricz, apiCfg.handlerHitCount)
+	mux.HandleFunc(filepathResetMetricz, apiCfg.handlerResetHitCount)
+
 	server := &http.Server{
 		Addr:    "localhost:" + port,
 		Handler: mux,
 	}
-	return server
-}
 
-func startServer(server *http.Server) {
-	server.ListenAndServe()
+	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
+	log.Fatal(server.ListenAndServe())
 }
